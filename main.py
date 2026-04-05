8af82f2425e929053298b0a416c26647e57bde87
Ravindu Wajira <129941007+ravinduwajira@users.noreply.github.com>
2026-04-05 02:07:08 +0000

    Add __init__ method to DataAnalyzer class

--- main.py
+++ main.py
@@ -7,4 +7,7 @@ import numpy as np
 from sklearn.datasets import load_iris, load_wine
 
 class DataAnalyzer:
-    pass
+    def __init__(self, dataset_name, data, feature_names):
+        self.dataset_name = dataset_name
+        self.data = pd.DataFrame(data, columns=feature_names)
+        self.numerical_attributes = feature_names
